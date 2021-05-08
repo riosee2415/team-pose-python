bd500043b075bd9f94ff3079d4652c606f943620
4LEAF.YSH <4leaf.ysh@gmail.com>
2021-05-08 18:59:29 +0900

    a

--- members/views.py
+++ members/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from . import models as member_models
 
 
 def member_list_view_handler(request):
-    return render(request, "screens/member_list.html")
+
+    members = member_models.MemberModel.objects.all()
+
+    return render(request, "screens/member_list.html", context={"members": members})
